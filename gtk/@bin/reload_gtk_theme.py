7ab134bc1831d42d96655cc5fb319944401275d9
oopsy <whoops.yao@gmail.com>
2021-08-05 15:51:07 +0800

    use a script to set GTK theme
    Read GTK setting from one place, and derives settings
    for other config files.

---
+++ gtk/@bin/reload_gtk_theme.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import os
+import yaml
+import between_lines as bl
+
+class ConfigError(Exception):
+    pass
+
+with open(os.path.expanduser('~/.config/gtk/config.yml'), 'r') as f:
+    config = yaml.safe_load(f)
+
+    theme = config.get('theme')
+    icon = config.get('icon')
+    cursor = config.get('cursor')
+    if None in [theme, icon, cursor]:
+        raise ConfigError('Invalid configuration')
+
+
+# GTK 2/3
+def gtk():
+    content = '\n'.join([
+        f'gtk-theme-name="{theme}"',
+        f'gtk-icon-theme-name="{icon}"',
+        f'gtk-cursor-theme-name="{cursor}"',
+    ])
+    bl.write('~/.config/gtk-3.0/settings.ini', content, '#')
+    bl.write('~/.gtkrc-2.0', content, '#')
+
+
+
+# XSETTINGSD
+def xsettingsd():
+    content = '\n'.join([
+        f'Net/ThemeName "{theme}"',
+        f'Net/IconThemeName "{icon}"',
+        f'Gtk/CursorThemeName "{cursor}"',
+    ])
+    bl.write('~/.xsettingsd', content, '#')
+    os.system('killall -HUP xsettingsd')
+
+
+# GSETTINGS
+def gsettings():
+    # TODO check if gsettings command available
+    gnome_schema = 'org.gnome.desktop.interface'
+    for k, v in [
+        ('gtk-theme', theme),
+        ('icon-theme', icon),
+        ('cursor-theme', cursor)
+    ]:
+        os.system(f'gsettings set {gnome_schema} {k} {v}')
+
+
+# CURSOR
+def cursor_special():
+    content = f'Inherits={cursor}'
+    bl.write('~/.icons/default/index.theme', content, '#')
+
+
+gtk()
+xsettingsd()
+gsettings()
+cursor_special()
