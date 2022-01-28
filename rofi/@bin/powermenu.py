4079440905d51446ce513f42b39b28fa57361344
oopsy <whoops.yao@gmail.com>
2022-01-28 16:49:31 +0800

    hibernate and hibernate-win

--- rofi/@bin/powermenu.py
+++ rofi/@bin/powermenu.py
@@ -3,9 +3,10 @@ import os
 
 options = {
     'sleep': ' Sleep',
-    'hibernate': '  Hibernate',
+    'hibernate-win': '  Hibernate (Win)',
     'reboot': ' Reboot',
-    'shutdown': ' Shutdown'
+    'shutdown': ' Shutdown',
+    'hibernate': '  Hibernate',
 }
 
 options_stdin = '\n'.join(options.values())
@@ -13,9 +14,11 @@ chosen = os.popen(f"echo '{options_stdin}'"
                   f'| rofi -dmenu -i -p "LET\'S"'
                   f' -theme powermenu').read().strip()
 if chosen == options['sleep']:
-    os.system('systemctl suspend')
-elif chosen == options['hibernate']:
+    os.system('systemctl suspend-then-hibernate')
+elif chosen == options['hibernate-win']:
     os.system('sudo grub-reboot Windows && systemctl hibernate')
+elif chosen == options['hibernate']:
+    os.system('systemctl hibernate')
 elif chosen == options['reboot']:
     os.system('systemctl reboot')
 elif chosen == options['shutdown']:
