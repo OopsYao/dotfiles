41db97391094f6da4fe2a286849997d4bf3a151d
oopsy <whoops.yao@gmail.com>
2023-01-01 23:12:27 +0800

    gitsigns setting for git operation

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -49,7 +49,23 @@ require('packer').startup({function()
   use {
     'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
     config = function()
-      require'gitsigns'.setup()
+      require'gitsigns'.setup {
+        current_line_blame = true,
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+          map('n', '<leader>hp', gs.preview_hunk)
+          map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+          map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+          map('n', '<leader>hu', gs.undo_stage_hunk)
+          map('n', '<leader>hS', gs.stage_buffer)
+          map('n', '<leader>hR', gs.reset_buffer)
+        end,
+      }
     end,
   }
   use 'psliwka/vim-smoothie' -- Smooth scrolling
