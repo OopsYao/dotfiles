cab6e23389acee4f97dd580c528aa5c84e51791d
oopsy <whoops.yao@gmail.com>
2023-08-10 11:39:45 +0800

    fix nvim-cmp config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -39,38 +39,64 @@ require("packer").startup {
         vim.o.completeopt = "menuone,noselect"
         local cmp = require "cmp"
         local luasnip = require "luasnip"
+        local has_words_before = function()
+          unpack = unpack or table.unpack
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+        end
+        local function super_tab(fallback)
+          -- This will be called even not selected if the snippet trigger matches.
+          -- In other words, cmp.confirm calls luasnip.expand internally.
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          elseif fallback ~= nil then
+            fallback()
+          end
+        end
+
         cmp.setup {
           snippet = {
             expand = function(args)
               require("luasnip").lsp_expand(args.body)
             end,
           },
-          sources = {
+          sources = cmp.config.sources({
+            { name = "luasnip" },
             { name = "nvim_lsp" },
             { name = "nvim_lua" },
-            { name = "luasnip" },
+          }, {
+            { name = "buffer" },
+          }),
+          window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
           },
           mapping = cmp.mapping.preset.insert {
             ["<C-b>"] = cmp.mapping.scroll_docs(-4),
             ["<C-f>"] = cmp.mapping.scroll_docs(4),
-            ["<CR>"] = cmp.mapping.confirm { select = true },
-            ["<Tab>"] = cmp.mapping(function(fallback)
-              if luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
+            ["<CR>"] = cmp.mapping(function(fallback)
+              if cmp.visible() and cmp.get_active_entry() then
+                cmp.confirm { select = true }
               else
                 fallback()
               end
-            end, { "i", "s" }),
-
+            end),
+            ["<Tab>"] = cmp.mapping(super_tab, { "i", "s" }),
             ["<S-Tab>"] = cmp.mapping(function(fallback)
-              if luasnip.jumpable(-1) then
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
                 luasnip.jump(-1)
               else
                 fallback()
               end
-            end, { "i", "s" }),
+            end),
           },
         }
+        -- Seems like that nvim-cmp doesnot support mapping in normal mode
+        vim.keymap.set("n", "<Tab>", super_tab)
       end,
       -- cmp-nvim-lsp provides the LSP source
       requires = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-nvim-lua", "saadparwaiz1/cmp_luasnip" },
--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -2,15 +2,55 @@ local ls = require "luasnip"
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local extras = require "luasnip.extras"
 
 ls.add_snippets("tex", {
   s("tab", {
-    t { "\\begin{table}[htbp]", "\t" },
-    t { "\\centering", "\t" },
-    i(1),
-    t { "\t", "\\caption{" },
-    i(2),
+    t "\\begin{table}",
+    i(1, "[htbp]"),
+    t { "", "\t\\centering", "\t\\caption{" },
+    i(3),
     t { "}", "\t" },
-    t "\\end{table}",
+    i(2),
+    t { "", "\\end{table}" },
+  }),
+  s("fig", {
+    t "\\begin{figure}",
+    i(1, "[htbp]"),
+    t { "", "\t\\centering", "\t" },
+    i(2),
+    t { "", "\t\\caption{" },
+    i(3),
+    t { "}", "\\end{figure}" },
+  }),
+  s("beg", {
+    t "\\begin{",
+    i(1),
+    t "}",
+    t { "", "\t" },
+    i(2),
+    t { "", "\\end{" },
+    extras.rep(1),
+    t "}",
+  }),
+  s("mul", {
+    t { "\\[\\begin{split}", "\t" },
+    i(1),
+    t { "", "\\end{split}\\]" },
+  }),
+  s("mulb", {
+    t { "\\[", "\t\\left\\{\\begin{aligned}", "\t" },
+    i(1),
+    t { "", "\t\\end{aligned}\\right.", "\\]" },
+  }),
+  s("~ci", {
+    t "~\\cite{",
+    i(1),
+    t "}",
+  }),
+  s("lb", {
+    t "\\label{",
+    i(1),
+    t "}",
   }),
 })
